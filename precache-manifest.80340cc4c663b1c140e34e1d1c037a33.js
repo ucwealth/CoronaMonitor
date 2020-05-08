25136876fcd8834befdcc45b811b2004f68b55d4
ucwealth <ucheokeke16@gmail.com>
2020-05-08 09:30:52 +0100

    Updates

---
+++ precache-manifest.80340cc4c663b1c140e34e1d1c037a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145377b17dbdfbe98c599ac898e88417",
+    "url": "/SCAMP-Assessment/index.html"
+  },
+  {
+    "revision": "3b24167eab0c22e19d17",
+    "url": "/SCAMP-Assessment/static/css/2.e2ffb87f.chunk.css"
+  },
+  {
+    "revision": "5352cefc8c8c8fee1bc0",
+    "url": "/SCAMP-Assessment/static/css/main.44af324f.chunk.css"
+  },
+  {
+    "revision": "3b24167eab0c22e19d17",
+    "url": "/SCAMP-Assessment/static/js/2.7fe0c7c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SCAMP-Assessment/static/js/2.7fe0c7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5352cefc8c8c8fee1bc0",
+    "url": "/SCAMP-Assessment/static/js/main.469f72e9.chunk.js"
+  },
+  {
+    "revision": "c2725bb5fcf8992cd708",
+    "url": "/SCAMP-Assessment/static/js/runtime-main.4b6a7a17.js"
+  }
+]);
